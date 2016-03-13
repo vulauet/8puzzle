0297e5325423d13bcd08468e87c70a1a0a4a308f
vulauet <vulauet@github.com>
2016-03-13 07:11:33 +0700

    Pass all tests

--- Board.java
+++ Board.java
@@ -13,14 +13,14 @@ public class Board {
         sizeSquare = size*size;
         board = new int[sizeSquare];
         for (int i = 0; i < size; i++)
-            for (int j=0; j < size; j++) 
+            for (int j = 0; j < size; j++) 
                 board[i*size + j] = blocks[i][j];                
     }         
     
     private Board makeBoard(int[] blocks) {
         int[][] newBoard = new int[size][size];
-        for (int i=0; i<size; i++)
-            for (int j=0; j<size; j++)
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
                 newBoard[i][j] = blocks[i*size + j];
         return new Board(newBoard);
     }
@@ -31,53 +31,50 @@ public class Board {
     // number of blocks out of place
     public int hamming() { 
         int score = 0;
-        for (int i=0; i<sizeSquare; i++) if (board[i] != i+1 && board[i] != 0) score++;
+        for (int i = 0; i < sizeSquare; i++) if (board[i] !=  i+1 && board[i] !=  0) score++;
         return score;
     }                  
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
         int score = 0;
-        for (int i=0; i<sizeSquare; i++) 
-			if (board[i] != 0) 
-				score += Math.abs((board[i]-1)/size - i/size) + Math.abs((board[i]-1)%size - i%size);
+        for (int i = 0; i < sizeSquare; i++) if (board[i] !=  0) score +=  Math.abs((board[i]-1)/size - i/size) + Math.abs((board[i]-1) % size - i % size);
         return score;
     }                
 
     // is this board the goal board?
     public boolean isGoal() {
-        for (int i=0; i<sizeSquare; i++) if (board[i] != i+1 && board[i] != 0) return false;
-        return true;
+        return hamming() == 0;
     }               
 
     // a board that is obtained by exchanging any pair of blocks
     public Board twin() {
         int[][] blocks = new int[size][size];
         int index = 0;
-        for (int i = 0; i<size; i++) {
-            for (int j=0; j<size; j++ ) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 blocks[i][j] = board[i*size + j];
-                if (board[i*size + j] == 0) index = i*size + j;
+                if (board[i*size + j] ==  0) index = i*size + j;
             }
         }
         int i = 0;
-        while (i == index || i+1 == index || (i+1)/size - i/size > 0) i++; 
-		// StdOut.println(i + ": " + blocks[i/size][i%size]);
-		// StdOut.println(i + 1 + ": " + blocks[i/size + 1][i%size]);
-        int tmp = blocks[i/size][i%size];
-        blocks[i/size][i%size] = blocks[i/size][i%size+1];
-        blocks[i/size][i%size+1] = tmp;
+        while (i ==  index || i+1 ==  index || (i+1)/size - i/size > 0) i++; 
+		// StdOut.println(i + ": " + blocks[i/size][i % size]);
+		// StdOut.println(i + 1 + ": " + blocks[i/size + 1][i % size]);
+        int tmp = blocks[i/size][i % size];
+        blocks[i/size][i % size] = blocks[i/size][i % size+1];
+        blocks[i/size][i % size+1] = tmp;
         return new Board(blocks);
     }                   
 
     // does this board equal y?
     public boolean equals(Object y) {
-        if (y == this) return true;
-        if (y == null) return false;
-        if (y.getClass() != this.getClass()) return false;
+        if (y ==  this) return true;
+        if (y ==  null) return false;
+        if (y.getClass() !=  this.getClass()) return false;
         Board other = (Board) y;
-		if (dimension() != other.dimension()) return false;
-        for (int i=0; i<sizeSquare; i++) if (board[i] != other.board[i]) return false;
+		if (dimension() !=  other.dimension()) return false;
+        for (int i = 0; i < sizeSquare; i++) if (board[i] !=  other.board[i]) return false;
         return true; 
     }       
 
@@ -88,7 +85,7 @@ public class Board {
         int[] tmpBoard = new int[sizeSquare];
         copyBoard(board, tmpBoard);
         int i = zeroIndex/size;
-        int j = zeroIndex%size;
+        int j = zeroIndex % size;
         if (valid(i, j+1)) nb.push(makeBoard(swap(board, zeroIndex, zeroIndex+1)));
         if (valid(i, j-1)) nb.push(makeBoard(swap(board, zeroIndex, zeroIndex-1)));
         if (valid(i+1, j)) nb.push(makeBoard(swap(board, zeroIndex, zeroIndex+size)));
@@ -97,17 +94,17 @@ public class Board {
     }    
 
     private int findZero() { 
-        for (int i = 0; i<sizeSquare; i++) if (board[i] == 0) return i;
+        for (int i = 0; i < sizeSquare; i++) if (board[i] ==  0) return i;
 		return -1;
     }
 
-    private void copyBoard(int[] src, int[] des) { for (int i = 0; i<sizeSquare; i++) des[i] = src[i]; }
+    private void copyBoard(int[] src, int[] des) { for (int i = 0; i < sizeSquare; i++) des[i] = src[i]; }
 
-    private boolean valid(int x, int y) { return x >= 0 && x < size && y >= 0 && y < size; }
+    private boolean valid(int x, int y) { return x >=  0 && x < size && y >=  0 && y < size; }
 
     private int[] swap(int[] orig, int index1, int index2) {
         int[] newBoard = new int[sizeSquare];
-        for (int i=0; i<sizeSquare; i++) newBoard[i] = orig[i];
+        for (int i = 0; i < sizeSquare; i++) newBoard[i] = orig[i];
         int tmp = newBoard[index1];
         newBoard[index1] = newBoard[index2];
         newBoard[index2] = tmp;
@@ -117,8 +114,8 @@ public class Board {
     // string representation of this board (in the output format specified below)
     public String toString() {
         String result = size + "\n";
-        for (int i=0; i<size; i++ ) {
-            for (int j=0; j<size ; j++ ) {
+        for (int i = 0; i < size; i++ ) {
+            for (int j = 0; j < size ; j++ ) {
                 result = result + board[i*size + j] + " ";
             }
             result = result + "\n";
--- Solver.java
+++ Solver.java
@@ -1,27 +1,105 @@
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.MinPQ;
 
 public class Solver {
+	private int numOfMove;
+	private boolean solvable;
+	private MinPQ<Node> openSet;
+	private MinPQ<Node> twinSet;
+	private Node start;
+	private Node last;
+	private Node evilTwin;
+	private Stack<Board> step;
+	
+	private class Node implements Comparable<Node> {
+		int step = 0;
+		Board board;
+		Node prev;
+		int fScore;
+		
+		Node(Board b, int stepMade, Node prevNode) {
+			board = b;
+			step = stepMade;
+			prev = prevNode;
+			fScore = step + b.manhattan();
+		}
+		
+		public int compareTo(Node other) {
+			if (fScore < other.fScore) return -1;
+			else if (fScore > other.fScore) return 1;
+			return 0;
+		}
+	}
+	
     public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
     {
-
+		openSet = new MinPQ<Node>();
+		twinSet = new MinPQ<Node>();
+		solvable = false;
+		numOfMove = -1;
+		start = new Node(initial, 0, null);
+		evilTwin = new Node(start.board.twin(), 0, null);
+		openSet.insert(start);
+		twinSet.insert(evilTwin);
+		Node current;
+		Node newNeighbor;
+		Node alternateCurrent;
+		
+		while (!openSet.isEmpty() || !twinSet.isEmpty()) {
+			current = openSet.delMin();
+			alternateCurrent = twinSet.delMin();
+			
+			if (current.board.isGoal()) {
+				solvable = true;
+				last = current;
+				break;
+			}
+			
+			if (alternateCurrent.board.isGoal()) {
+				break;
+			}
+			
+			for (Board b : current.board.neighbors()) {
+				if (current.prev != null && b.equals(current.prev.board)) continue;
+				newNeighbor = new Node(b, current.step + 1, current);
+				openSet.insert(newNeighbor);
+			}
+			
+			for (Board b : alternateCurrent.board.neighbors()) {
+				if (alternateCurrent.prev != null && b.equals(alternateCurrent.prev.board)) continue;
+				newNeighbor = new Node(b, alternateCurrent.step + 1, alternateCurrent);
+				twinSet.insert(newNeighbor);
+			}
+		}
+		
+		if (solvable) {
+			step = new Stack<Board>();
+			Node cur = last;
+			while (cur != null) {
+				numOfMove++;
+				step.push(cur.board);
+				cur = cur.prev;
+			}
+		}
     }
     
     public boolean isSolvable()            // is the initial board solvable?
     {
-    	return true;
+    	return solvable;
     }
 
     public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
     {
-    	return -1;
+    	// if (numOfMove == 0) return -1;
+		return numOfMove;
     }
     
     public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
     {
-    	Stack<Board> st = new Stack<Board>();
-    	return st;
+		if (!isSolvable()) return null;
+    	return step;
     }
 
     public static void main(String[] args) // solve a slider puzzle (given below)
